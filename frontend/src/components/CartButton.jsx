a5f0912c84ae34479055250e1aa7c71ea3674aae
Katarzyna Pastula <katarzyna.pastula@outlook.com>
2023-10-17 17:56:46 +0200

    Header

--- frontend/src/components/CartButton.jsx
+++ frontend/src/components/CartButton.jsx
@@ -2,8 +2,6 @@ import "./CartButton.css"
 
 export default function CartButton(props) {
     return (
-        <div>
             <button className="cartButton" onClick={props.onClick}>🛒</button>
-        </div>
     )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import "./Header.css";
+import CartButton from "./CartButton.jsx";
 
 export default function Header() {
     return (
         <div className="header">
+            <div className="logo">
             <img src="https://img.freepik.com/premium-vector/women-lingerie-pink-body-corset-fashion-concept_277625-1447.jpg?w=2000" alt="logo"></img>
-            <div>
+                <div>
                 <h1>IntimateCode</h1>
                 <h2>"Code Your Imagination, Create Reality"</h2>
+                </div>
             </div>
-
-            <button></button>
+            <div className="header-buttons">
+            <button className="loginButton"><p className="loginButtonIcon">👤</p></button>
+            <CartButton />
+            </div>
         </div>
     )
 }
