3b5a78f1ff20633c3d683b1a96721d0a70be5ee0
Katarzyna Pastula <katarzyna.pastula@outlook.com>
2023-11-09 18:54:47 +0100

    StarRating

---
+++ frontend/src/components/starRating/StarRating.jsx
@@ -0,0 +1,41 @@
+// TODO: figure out when to set productRating? if the user is logged in and already bought the product? 
+
+
+import { useState } from "react";
+import "./StarRating.css";
+import { FaStar } from "react-icons/fa";
+
+export default function StarRating({productRating, userType}) {
+  const [rating, setRating] = useState(productRating);
+  const [rateColor, setRateColor] = useState(null);
+
+  console.log(productRating);
+
+  return (
+    <div className="starRating">
+      {[...Array(5)].map((star, index) => {
+        const currentRate = index + 1;
+
+        return (
+          <label key={index}>
+            <input
+              type="radio"
+              className="rate"
+              value={currentRate}
+              onClick={() => {
+                if (userType === "loggedIn") {
+                setRating(currentRate);
+                setRateColor(currentRate);
+                }
+              }}
+            />
+            <FaStar
+              size={20}
+              color={currentRate <= (rateColor || rating) ? "#f63081" : "#f6308337"}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+}
